276d833679d08595885e03f00e33d23744c25a40
_lucasborges <lucas.pborges@yahoo.com.br>
2020-06-17 19:07:25 -0300

    Project created

---
+++ backend/customer-relationship/src/test/java/br/com/customerrelationship/CustomerRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.customerrelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
