424efe1ea544775c0e1079eb731ac748cdfdc1ff
_lucasborges <lucas.pborges@yahoo.com.br>
2020-06-22 19:40:20 -0300

    refactor folders names and methods names

--- backend/customer-relationship/src/main/java/br/com/customerrelationship/security/CorsConfiguration.java
+++ backend/customer-relationship/src/main/java/br/com/customerrelationship/security/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package br.com.customerrelationship.Security;
+package br.com.customerrelationship.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
