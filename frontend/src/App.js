2eb5a285f411aabe5118b5552cf8d8d4b441efc7
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-20 20:41:57 -0300

    add edit page style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home'
 import Register from './pages/Register';
+import CustomerDetails from './pages/CustomerDetails';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={ Home }/>
             <Route path="/register" component={ Register }/>
+            <Route path="/edit" component={ CustomerDetails }/>
           </Switch>
       </Router>
     </React.Fragment>
--- frontend/src/pages/CustomerDetails.js
+++ frontend/src/pages/CustomerDetails.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import Header from '../components/Header/Header';
+import './home.css';
+import './customerDetails.css';
+import Input from '../components/Input/Input';
 
 export default class CustomerDetails extends Component {
 
     render() {
 
         return (
-            <div>
+            <div className="containerHome">
                 <Header
                     divCabecalho="cabecalho"
                     divTitulo="tituloCabecalho"
                     divBtn="divButtons"
                 />
-                <div className="divLeft"></div>
-                <div className="divCustomer"></div>
+                <div className="editBody">
+                <div className="divLeft">
+                    <label>Name:</label>
+                    <label>Birth Date:</label>
+                    <label>Email:</label>
+                    <label>Telephone:</label>
+                    <label>Address:</label>
+                    <label>Number:</label>
+                    <label>Complement:</label>                    
+                </div>
+                <div className="divCustomer">
+                    <Input nameClass="inputEdit"/>
+                    <Input nameClass="inputEdit"/>
+                    <Input nameClass="inputEdit"/>
+                    <Input nameClass="inputEdit"/>
+                    <Input nameClass="inputEdit"/>
+                    <Input nameClass="inputEdit"/>
+                    <Input nameClass="inputEdit"/>
+                    <div className="btnDivEdit">
+                        <Input nameClass="btnEdit" tipo="submit" value="Save"/>
+                        <Input nameClass="btnEdit" tipo="submit" value="Return"/>
+                    </div>
+                </div>
+                </div>
                 <div className="divFot"></div>
             </div>
         )
