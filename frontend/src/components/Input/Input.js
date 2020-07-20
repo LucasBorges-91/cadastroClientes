b53aaba6fa834dc7d25789d304702116d0cd7f28
LucasBorges-91 <59208135+LucasBorges-91@users.noreply.github.com>
2020-07-20 16:49:36 -0300

    Update frontend/src/components/Input/Input.js
    Co-authored-by: Havyner Caetano <20730250+HCaetano@users.noreply.github.com>

--- frontend/src/components/Input/Input.js
+++ frontend/src/components/Input/Input.js
@@ -6,7 +6,8 @@ export default class Input extends Component {
     render() {
         const { nameClass, placeholder, type, value, func, funct } = this.props
         return (
-            <input className={nameClass} type={tipo} value={value} placeholder={placeholder} onBlur={func} onMouseDown={funct}/> 
+            <input className={nameClass} type={type} value={value} placeholder={placeholder} onBlur={func} onMouseDown={funct}/> 
+
         );
     }
 }
